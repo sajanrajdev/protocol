bac5fafbaee85493c2296149e61a6d5662d75cda
Alberto Cevallos <acvllsh@gmail.com>
2021-09-27 08:17:26 -0400

    adding natspec coverage for getters, external and mutatuve funcs

--- contracts/Quad.sol
+++ contracts/Quad.sol
@@ -45,6 +45,15 @@ import { IWETH } from "../interfaces/erc20/IWETH.sol";
 //   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 //   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 
+/**
+ * @title Quads
+ * @author Quads Finance Team
+ *
+ * Upgradable contract for minting and burning index fund tokens (quads), using any ERC-20 token
+ * as the paying currency.
+ *
+ */
+
 contract Quad is PausableUpgradeable, ERC20Upgradeable {
   using SafeERC20Upgradeable for IERC20Upgradeable;
   using AddressUpgradeable for address;
@@ -92,13 +101,14 @@ contract Quad is PausableUpgradeable, ERC20Upgradeable {
     string memory _namePrefix,
     string memory _symbolPrefix
   ) public initializer whenNotPaused {
-    __Pausable_init(); // added now
+    __Pausable_init();
     governance = _governance;
     manager = _manager;
     tokens = _tokensConfig;
     weights = _weightsConfig;
     units = _unitsConfig;
     inputs = _inputsConfig;
+
     // Token
     string memory name;
     string memory symbol;
@@ -129,11 +139,23 @@ contract Quad is PausableUpgradeable, ERC20Upgradeable {
 
   /* ========== VIEWS ========== */
 
-  /// @dev Specify the version of the Quad, for upgrades
+  /**
+   * Specifies the version of the Quad for future upgrades.
+   *
+   * @return    String containing version of the Quad deployment.
+   */
   function version() external pure returns (string memory) {
-    return "0.1.1";
+    return "0.1.0";
   }
 
+  /**
+   * Returns an estimated amount of QuadTokens that can be issued given an amount of input ERC20 token.
+   *
+   * @param _inputToken       Address of input token.
+   * @param _amountInput      Amount of the input token to spend
+   *
+   * @return                  Estimated amount of QuadTokens that will be received
+   */
   function getEstimatedQuadsGivenInput(
     address _inputToken,
     uint256 _amountInput
@@ -187,6 +209,15 @@ contract Quad is PausableUpgradeable, ERC20Upgradeable {
 
   /* ========== MUTATIVE FUNCTIONS ========== */
 
+  /**
+   * Issues QuadTokens for an exact amount of input ERC20 tokens.
+   * The ERC20 token must be approved by the sender to this contract.
+   * This method does not return any data.
+   *
+   * @param _token       Address of input token
+   * @param _amount      Amount of the input token / ether to spend
+   * @param _quantity    Minimum amount of SetTokens to receive. Prevents unnecessary slippage.
+   */
   function mint(
     address _token,
     uint256 _amount,
@@ -218,6 +249,14 @@ contract Quad is PausableUpgradeable, ERC20Upgradeable {
     _mint(msg.sender, shares);
   }
 
+  /**
+   * Burns the QuadToken's positions and sends the components of the given
+   * quantity to the caller.
+   * This method does not return any data.
+   *
+   * @param _shares             Quantity of the QuadToken to burn and redeem
+   */
+
   function burn(uint256 _shares) public whenNotPaused {
     /// @dev Security implementations
     _defend();
@@ -243,51 +282,99 @@ contract Quad is PausableUpgradeable, ERC20Upgradeable {
 
   /* ========== RESTRICTED FUNCTIONS ========== */
 
-  /// @notice Change tokens array
-  /// @notice Can only be changed by governance
+  /**
+   * Changes the tokens array, can only be called by governance.
+   * The tokens state contains the list of ERC-20 constituting each Quad.
+   * Tokens should not be altered in v0.1.0.
+   *
+   * @param _tokens             Array of token addresses updating the `tokens` state.
+   */
   function setTokens(address[] memory _tokens) external whenNotPaused {
     _onlyGovernance();
     tokens = _tokens;
   }
 
-  /// @notice Change weights array
-  /// @notice Can only be changed by governance
+  /**
+   * Changes the weights array, can only be called by governance.
+   * The weights state dictates the "weight" of each token in the tokens list in our Quad contract.
+   * Weights should not be altered in v0.1.0.
+   *
+   * @param _weights             Array of uint256 weights updating the `weights` state.
+   */
   function setWeights(uint256[] memory _weights) external whenNotPaused {
     _onlyGovernance();
     weights = _weights;
   }
 
-  /// @notice Change units array
-  /// @notice Can only be changed by governance
-  function setUnits(uint256[] memory _inputs) external whenNotPaused {
+  /**
+   * Changes the units array, can only be called by governance.
+   * The units state defines (in uint256) the amount of each token in tokens, needed to issue a single share.
+   * Units should not be altered in v0.1.0.
+   *
+   * @param _units             Array of uint256 units updating the `units` state.
+   */
+  function setUnits(uint256[] memory _units) external whenNotPaused {
     _onlyGovernance();
-    units = _inputs;
+    units = _units;
   }
 
-  /// @notice Change inputs array
-  /// @notice Can only be changed by governance
+  /**
+   * Changes the inputs array, can only be called by governance.
+   * The inputs state restricts the type of tokens that can be used to purchase a share.
+   * Currently, in v0.1.0, only DAI, USDT and USDC are supported. However, this contract can support any token in the router.
+   * Inputs should not be altered in v0.1.0.
+   *
+   * @param _inputs             Array of token addresses updating the `inputs` state.
+   */
   function setInputs(address[] memory _inputs) external whenNotPaused {
     _onlyGovernance();
     inputs = _inputs;
   }
 
+  /**
+   * Allows governance or manager to pause the contract.
+   * When paused, methods with whenNotPaused, will revert upon call.
+   */
   function pause() external {
     _onlyAuthorizedPausers();
     _pause();
   }
 
+  /**
+   * Only allows governance to unpause the contract.
+   * When unpaused, methods with whenNotPaused, will not revert upon call.
+   */
   function unpause() external {
     _onlyGovernance();
     _unpause();
   }
 
+  /**
+   * Allows governance to rescue any ERC-20 token sent accidentally to this contract.
+   * Method does not allow governance to withdraw any token in the tokens state.
+   * Recovered tokens are always sent to the governance multisig.
+   *
+   * @param tokenAddress             Address of token being recovered.
+   * @param tokenAmount              Amount (in uint256) being recovered.
+   */
   function recoverERC20(address tokenAddress, uint256 tokenAmount) external {
     _onlyGovernance();
+    for (uint256 i = 0; i < tokens.length; i++) {
+      require(tokenAddress != tokens[i], "Asset is protected");
+    }
     IERC20Upgradeable(tokenAddress).safeTransfer(governance, tokenAmount);
   }
 
   /* ========== INTERNAL FUNCTIONS ========== */
 
+  /**
+   * Sets a max approval limit for an ERC20 token, provided the current allowance
+   * is less than the required allowance.
+   *
+   * @param _token    Token to approve
+   * @param _spender  Spender address to approve
+   * @param _requiredAllowance  Allowance necessary for transaction
+   */
   function _safeApprove(
     address _token,
     address _spender,
@@ -305,6 +392,17 @@ contract Quad is PausableUpgradeable, ERC20Upgradeable {
     }
   }
 
+  /**
+   * Returns number of QuadTokens to mint, given an exact amount of input ERC20 tokens.
+   * The ERC20 token must be approved by the sender to this contract.
+   * This method also swaps the input ERC20 for each component in the Quad tokens state.
+   *
+   * @param _inputToken       Address of input token
+   * @param _amountInput      Amount of the input token / ether to spend
+   * @param _minQuadReceive   Minimum amount of QuadTokens to receive. Prevents unnecessary slippage.
+   *
+   * @return quadIssueAmount  Amount of SetTokens issued to the caller
+   */
   function _calculateQuadForExactToken(
     address _inputToken,
     uint256 _amountInput,
@@ -320,11 +418,6 @@ contract Quad is PausableUpgradeable, ERC20Upgradeable {
     uint256 amountEth = _inputToken == WETH
       ? _amountInput
       : _swapTokenForWETH(_inputToken, _amountInput);
-
-    // uint256 quadTokenAmount = _issueQuadForExactWETH(
-    //   _minQuadReceive,
-    //   amountEth
-    // );
 
     address[] memory components = tokens;
     uint256 sumEth = 0;
